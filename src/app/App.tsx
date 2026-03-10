358b5306f2a65c89529b53b1fec2f82d6d6384ab
Mousab <suppors249@gmail.com>
2026-03-10 06:13:49 +0200

    Prepare Vercel deploy with Node 24

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 import { HomePage } from "./pages/HomePage";
 import { ServicesPage } from "./pages/ServicesPage";
@@ -19,6 +20,7 @@ export default function App() {
 
       {/* ScrollToTop ensures new pages open from top */}
       <ScrollToTop />
+      <LoadingScreen />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
